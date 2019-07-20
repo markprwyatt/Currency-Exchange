9375fcabcc5892c3dc25994f701ef85dc22c9f40
Mark Wyatt <markprwyatt@gmail.com>
2019-07-20 16:21:58 +0100

    Updates

---
+++ precache-manifest.d95875416208244c1e4b19be2953fd9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2f610560cc340ef645d24d66a7ca32",
+    "url": "/markprwyatt/Currency-Exchange/index.html"
+  },
+  {
+    "revision": "604f10a0d1b653fa4a59",
+    "url": "/markprwyatt/Currency-Exchange/static/css/main.34b0f426.chunk.css"
+  },
+  {
+    "revision": "ba8e812d7d6a68cd14c7",
+    "url": "/markprwyatt/Currency-Exchange/static/js/2.74207875.chunk.js"
+  },
+  {
+    "revision": "604f10a0d1b653fa4a59",
+    "url": "/markprwyatt/Currency-Exchange/static/js/main.23c1755d.chunk.js"
+  },
+  {
+    "revision": "d4fe54f9dc7d22ede6e8",
+    "url": "/markprwyatt/Currency-Exchange/static/js/runtime~main.33ef3a05.js"
+  }
+]);
